8fb2aa246dd3cd6d1e6bd8487c840b10fe361930
Chris Fasbinder <fasbincd.uc@gmail.com>
2015-04-03 17:44:50 -0400

    Fully parses SSID AND Password
    Now pulls SSID and password from the html page and attaches terminal
    characters

--- TCU.py
+++ TCU.py
@@ -3,19 +3,29 @@ import time
 ser = serial.Serial('COM10', 9600, timeout=0)
 
 while 1:
-    time.sleep(1)
     incoming = ser.readline()
-    start = incoming.find(b'ssid=')
-    end = incoming.find(b'&')
-    outgoing = incoming[start+5:end+1] #'&' is SSID terminal character
-    print("SSID: %s" % outgoing)
-    start = incoming.find(b'pw=', end+1)
-    end = incoming.find(b'HTTP/1.1', start+3)
-    append = incoming[start+3:end-1]
-    outgoing = append + b'$' #'$' is Password terminal character
-    print("Password: %s\n" % outgoing)
+    temp = -1
+    temp = incoming.find(b'GET /?')
+    time.sleep(0.01)
+    if(temp != -1):
+        start = incoming.find(b'ssid=', temp)
+        end = incoming.find(b'&')
+        outgoing = incoming[start+5:end+1] #'&' is SSID terminal character
+        print("SSID: %s" % outgoing)
+        start = end + 4
+        end = incoming.find(b'HTTP/1.1', start)
+        append = incoming[start:end-1]
+        outgoing = append + b'$' #'$' is Password terminal character
+        print("Password: %s\n" % outgoing)
+#    else:
+#        if(incoming != b''):
+#            print(incoming)
 
-
+while 0:
+    test = ser.readline()
+    if(test != b''):
+        print(test)
+    time.sleep(.1)
 
 while 0:
     var = input("Enter something: ")
