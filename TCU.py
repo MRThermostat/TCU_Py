053ede9c1845474027e74bf9d2f6218b006463ae
Chris Fasbinder <fasbincd.uc@gmail.com>
2015-04-03 13:58:55 -0400

    SSID and password parsing
    Currently can parse out ssid and password from data sent by html page

--- TCU.py
+++ TCU.py
@@ -3,6 +3,21 @@ import time
 ser = serial.Serial('COM10', 9600, timeout=0)
 
 while 1:
+    time.sleep(1)
+    incoming = ser.readline()
+    start = incoming.find(b'ssid=')
+    end = incoming.find(b'&')
+    outgoing = incoming[start+5:end+1] #'&' is SSID terminal character
+    print("SSID: %s" % outgoing)
+    start = incoming.find(b'pw=', end+1)
+    end = incoming.find(b'HTTP/1.1', start+3)
+    append = incoming[start+3:end-1]
+    outgoing = append + b'$' #'$' is Password terminal character
+    print("Password: %s\n" % outgoing)
+
+
+
+while 0:
     var = input("Enter something: ")
     outgoing = var.encode('utf-8')
     time.sleep(.01)
