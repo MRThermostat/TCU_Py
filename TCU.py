92b0c9aebb04535f46b8c5f91c840ae58e2d7ad6
Chris Fasbinder <fasbincd.uc@gmail.com>
2015-04-04 23:24:28 -0400

    Parses Profile and Rules from json
    Currently parses profile name and rules from example json file. Prints
    in nice format over serial, will change later when actually implemented.
    json.txt has example json in formats that can be used by python and
    arduino.
    
    If opened in notepad++, can go to view->user-defined dialogue and import
    the fold.xml. Once imported change the language to test. This will allow
    collapsing of text for easier viewing
    (TCU_py.ino has no use)

--- TCU.py
+++ TCU.py
@@ -1,8 +1,14 @@
 import serial
 import time
-ser = serial.Serial('COM10', 9600, timeout=0)
+import json
+import ast
 
-while 1:
+ARDUINO = 0
+
+if ARDUINO:
+    ser = serial.Serial('COM10', 9600, timeout=0)
+
+while 0:
     incoming = ser.readline()
     temp = -1
     temp = incoming.find(b'GET /?')
@@ -35,3 +41,33 @@ while 0:
     var = outgoing.decode()
     time.sleep(.01)
     print("Arduino:\n %s\n" % ser.readline())
+
+#Trying to parse json
+info = json.loads('{\r\n\t"fan":"on",\r\n\t"system":"heat",\r\n\t"profileList":[\r\n\t\t{\r\n\t\t\t"active":0,\r\n\t\t\t"profileRulesList":[\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":70,\r\n\t\t\t\t\t"start_condition":1400,\r\n\t\t\t\t\t"end_condition":2300,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Weekend"\r\n\t\t\t\t},\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":66,\r\n\t\t\t\t\t"start_condition":300,\r\n\t\t\t\t\t"end_condition":500,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Weekend"\r\n\t\t\t\t}\r\n\t\t\t],\r\n\t\t\t"name":"Weekend"\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"profileRulesList":[\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":66,\r\n\t\t\t\t\t"start_condition":100,\r\n\t\t\t\t\t"end_condition":700,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Work Week"\r\n\t\t\t\t},\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":72,\r\n\t\t\t\t\t"start_condition":800,\r\n\t\t\t\t\t"end_condition":1400,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Work Week"\r\n\t\t\t\t}\r\n\t\t\t],\r\n\t\t\t"name":"Work Week"\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":0,\r\n\t\t\t"profileRulesList":[\r\n\t\t\t],\r\n\t\t\t"name":"Vacation"\r\n\t\t}\r\n\t],\r\n\t"sensorList":[\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Porch",\r\n\t\t\t"temperature":55\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Basement",\r\n\t\t\t"temperature":65\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":0,\r\n\t\t\t"name":"Porch",\r\n\t\t\t"temperature":80\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Bedroom",\r\n\t\t\t"temperature":55\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Foyer",\r\n\t\t\t"temperature":50\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Kitchen",\r\n\t\t\t"temperature":50\r\n\t\t}\r\n\t]\r\n}')
+json_info = json.dumps(info)
+decoded_info = json.loads(json_info)
+
+profileList = ', '.join(map(str, decoded_info["profileList"]))
+profileRulesList = ast.literal_eval(profileList)
+profileCount = 0
+for rules in profileRulesList:
+    profileCount += 1
+    print("\nProfile \"%s\":" % rules["name"])
+    json_info = json.dumps(rules)
+    decoded_info = json.loads(json_info)
+    profileRule = ', '.join(map(str, decoded_info["profileRulesList"]))
+    profileRuleVals = ast.literal_eval(profileRule)
+    ruleCount = 0
+    for values in profileRuleVals:
+        ruleCount += 1
+        print("\tRule %d:" % ruleCount)
+        json_info = json.dumps(values)
+        decoded_info = json.loads(json_info)
+        conds = decoded_info
+        print("\t\tStart Conditions: %d" % conds["start_condition"])
+        print("\t\tEnd Conditions: %d" % conds["end_condition"])
+        print("\t\tSettings: %d" % conds["start_condition"])
+        print("\t\tProfile Name: %d" % conds["start_condition"])
+        print("\t\tType: %d" % conds["start_condition"])
+    
+sensorList = ', '.join(map(str, decoded_info["sensorList"]))    
---
+++ serve_page/serve_page.ino
@@ -0,0 +1,162 @@
+/**
+ * @example TCPServer.ino
+ * @brief The TCPServer demo of library WeeESP8266. 
+ * @author Wu Pengfei<pengfei.wu@itead.cc> 
+ * @date 2015.02
+ * 
+ * @par Copyright:
+ * Copyright (c) 2015 ITEAD Intelligent Systems Co., Ltd. \n\n
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version. \n\n
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "ESP8266.h"
+
+ESP8266 wifi(Serial1);
+
+void setup(void)
+{
+  Serial.begin(9600);
+  while(!Serial){
+    //do nothing 
+  }
+  Serial.print("setup begin\r\n");
+
+  pinMode(ESP_GPIO0,OUTPUT);
+  pinMode(ESP_GPIO2,OUTPUT);
+  pinMode(ESP_RESET,OUTPUT);
+  pinMode(ESP_CH_PD,OUTPUT);
+
+  digitalWrite(ESP_GPIO0,HIGH);
+  digitalWrite(ESP_GPIO2,HIGH);
+  digitalWrite(ESP_RESET,LOW);
+  digitalWrite(ESP_CH_PD,HIGH);
+  delay(100);
+  digitalWrite(ESP_RESET,HIGH);
+  
+  delay(1000);
+  wifi.restart();
+  delay(1000);
+
+  Serial.print("FW Version:");
+  Serial.println(wifi.getVersion().c_str());
+
+  if (wifi.setOprToSoftAP()) {
+    Serial.print("to station + softap ok\r\n");
+  } 
+  else {
+    Serial.print("to station + softap err\r\n");
+  }
+  wifi.setSoftAPParam("MR THERMOSTAT", "", 7, 0);
+
+  /*if (wifi.joinAP("TySock2.4G", "lolamani")) {
+    Serial.print("Join AP success\r\n");*/
+    Serial.print("IP: ");
+    Serial.println(wifi.getLocalIP().c_str());    
+  /*} 
+  else {
+    Serial.print("Join AP failure\r\n");
+  }*/
+
+  if (wifi.enableMUX()) {
+    Serial.print("multiple ok\r\n");
+  } 
+  else {
+    Serial.print("multiple err\r\n");
+  }
+
+  if (wifi.startTCPServer(80)) {
+    Serial.print("start tcp server ok\r\n");
+  } 
+  else {
+    Serial.print("start tcp server err\r\n");
+  }
+
+  if (wifi.setTCPServerTimeout(10)) { 
+    Serial.print("set tcp server timout 10 seconds\r\n");
+  } 
+  else {
+    Serial.print("set tcp server timout err\r\n");
+  }
+  Serial1.setTimeout(1000);
+
+  Serial.print("setup end\r\n");
+}
+
+int generateResponse(uint8_t* buffer) {
+  const uint8_t html[189] = "<html><h1>Mr Thermostat Setup</h1><form>Network SSID:<input type=\"text\" name=\"ssid\"><br>Password:<input type=\"password\" name=\"pw\"><br><br><input type=\"submit\" value=\"Submit\"></form></html>";
+  int pos;
+
+  for (pos = 0; pos < 188; pos++) {
+    buffer[pos] = html[pos];
+    delay(1);
+  }
+  buffer[pos] = 0;
+  return pos + 1;
+}
+
+void loop(void)
+{
+  pinMode(ESP_GPIO0,OUTPUT);
+  pinMode(ESP_GPIO2,OUTPUT);
+  pinMode(ESP_RESET,OUTPUT);
+  pinMode(ESP_CH_PD,OUTPUT);
+
+  digitalWrite(ESP_GPIO0,HIGH);
+  digitalWrite(ESP_GPIO2,HIGH);
+  digitalWrite(ESP_RESET,HIGH);
+  digitalWrite(ESP_CH_PD,HIGH);
+  
+  
+  uint8_t buffer[200] = {
+    0  };
+  uint8_t mux_id;
+  uint32_t len = wifi.recv(&mux_id, buffer, sizeof(buffer), 200);
+  //Serial.println("created vars");
+  if (len > 0) {
+    Serial.print("Status:[");
+    Serial.print(wifi.getIPStatus().c_str());
+    Serial.println("]");
+
+    Serial.print("Received from :");
+    Serial.print(mux_id);
+    Serial.print("[");
+    for(uint32_t i = 0; i < len; i++) {
+      Serial.print((char)buffer[i]);
+    }
+    Serial.print("]\r\n");
+    len = generateResponse(buffer);
+    if(wifi.send(mux_id, buffer, len)) {
+      Serial.print("send back ok\r\n");
+    } 
+    else {
+      Serial.print("send back err\r\n");
+    }
+
+    if (wifi.releaseTCP(mux_id)) {
+      Serial.print("release tcp ");
+      Serial.print(mux_id);
+      Serial.println(" ok");
+    } 
+    else {
+      Serial.print("release tcp");
+      Serial.print(mux_id);
+      Serial.println(" err");
+    }
+
+    Serial.print("Status:[");
+    Serial.print(wifi.getIPStatus().c_str());
+    Serial.println("]");
+  }
+}
+
+
