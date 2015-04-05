8484d8c110d377127a10c955bf34d2eed8f8c2d2
Chris Fasbinder <fasbincd.uc@gmail.com>
2015-04-05 16:12:02 -0400

    Comments
    Comments

--- TCU.py
+++ TCU.py
@@ -7,11 +7,12 @@ import itertools
 ARDUINO = 0
 JSON = 0
 CREATEJSON = 1
+NETWORKSETUP = 0
 
-if ARDUINO:
+if ARDUINO: #Enables the use of serial port
     ser = serial.Serial('COM10', 9600, timeout=0)
 
-while 0:
+while NETWORKSETUP: #Pulls SSID and password from HTML page
     incoming = ser.readline()
     temp = -1
     temp = incoming.find(b'GET /?')
@@ -29,14 +30,14 @@ while 0:
     else:
         if(incoming != b''):
             print(incoming)
-
-while 0:
+            
+while 0: #Simply reads the serial
     test = ser.readline()
     if(test != b''):
         print(test)
     time.sleep(.1)
 
-while 0:
+while 0: #Test to send stuff over serial
     var = input("Enter something: ")
     outgoing = var.encode('utf-8')
     time.sleep(.01)
@@ -45,21 +46,10 @@ while 0:
     time.sleep(.01)
     print("Arduino:\n %s\n" % ser.readline())
 
-#Trying to parse json
-if JSON:
+if JSON: #Parses incoming JSON file from Android app
+    #testing data
     info = json.loads('{\r\n\t"fan":"on",\r\n\t"system":"heat",\r\n\t"profileList":[\r\n\t\t{\r\n\t\t\t"active":0,\r\n\t\t\t"profileRulesList":[\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":70,\r\n\t\t\t\t\t"start_condition":1400,\r\n\t\t\t\t\t"end_condition":2300,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Weekend"\r\n\t\t\t\t},\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":66,\r\n\t\t\t\t\t"start_condition":300,\r\n\t\t\t\t\t"end_condition":500,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Weekend"\r\n\t\t\t\t}\r\n\t\t\t],\r\n\t\t\t"name":"Weekend"\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"profileRulesList":[\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":66,\r\n\t\t\t\t\t"start_condition":100,\r\n\t\t\t\t\t"end_condition":700,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Work Week"\r\n\t\t\t\t},\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":72,\r\n\t\t\t\t\t"start_condition":800,\r\n\t\t\t\t\t"end_condition":1400,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Work Week"\r\n\t\t\t\t}\r\n\t\t\t],\r\n\t\t\t"name":"Work Week"\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":0,\r\n\t\t\t"profileRulesList":[\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":70,\r\n\t\t\t\t\t"start_condition":1400,\r\n\t\t\t\t\t"end_condition":2300,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Vacation"\r\n\t\t\t\t},\r\n\t\t\t\t{\r\n\t\t\t\t\t"setting":66,\r\n\t\t\t\t\t"start_condition":300,\r\n\t\t\t\t\t"end_condition":500,\r\n\t\t\t\t\t"type":"Time",\r\n\t\t\t\t\t"Profile_Name":"Vacation"\r\n\t\t\t\t}\r\n\t\t\t],\r\n\t\t\t"name":"Vacation"\r\n\t\t}\r\n\t],\r\n\t"sensorList":[\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Porch",\r\n\t\t\t"temperature":55\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Basement",\r\n\t\t\t"temperature":65\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":0,\r\n\t\t\t"name":"Porch",\r\n\t\t\t"temperature":80\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Bedroom",\r\n\t\t\t"temperature":55\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Foyer",\r\n\t\t\t"temperature":50\r\n\t\t},\r\n\t\t{\r\n\t\t\t"active":1,\r\n\t\t\t"name":"Kitchen",\r\n\t\t\t"temperature":50\r\n\t\t}\r\n\t]\r\n}')
-#    incoming = 0
-#    while not incoming:
-#        incoming = ser.readline()
-#    temp = -1
-#    temp = incoming.find(b'GET /?')
-#    time.sleep(0.01)
-#    if(temp != -1):
-#        start = incoming.find(b'ssid=', temp)
-#        end = incoming.find(b'&')
-#        data =incoming[start+5:end]
-#        
-#    info = json.loads(data)
+
     json_info = json.dumps(info)
     decoded_info = json.loads(json_info)
 
@@ -103,7 +93,8 @@ if JSON:
         else:
             print("\tActive: False")
 
-if CREATEJSON:
+if CREATEJSON: #Creates JSON for Android app
+    #testing data block
     settings,start_cond,end_cond,types,profile_name = 72,800,1400,"Time","Work Week" #Rule structure
     active,name = 0,"Weekend" #Profile structure
     active_s,name_s,temperature = 1,"Porch",55 #Sensor structure
@@ -111,6 +102,8 @@ if CREATEJSON:
     ruleCount = 2
     sensorCount = 6
     profileCount = 3
+    #
+    
     profileList = []
     sensorList = []
     for _ in itertools.repeat(None, profileCount):
